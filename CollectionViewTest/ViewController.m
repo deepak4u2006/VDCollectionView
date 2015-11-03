e96b9087fde61bd2b9c8718292ed9bb739a1b9f4
Vishwa Deepak <vishwadeepak@admins-macbook-pro.local>
2015-11-03 12:31:24 +0530

    Added CollectionView and running properly

--- CollectionViewTest/ViewController.m
+++ CollectionViewTest/ViewController.m
@@ -24,4 +24,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark -
+#pragma mark UICollectionView Datasource/Delegate
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 25;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"vdCellID" forIndexPath:indexPath];
+    UILabel *label = (UILabel *)[cell.contentView viewWithTag:100];
+    label.text = [NSString stringWithFormat:@"%ld~%ld", (long)indexPath.row, (long)indexPath.section];
+    cell.backgroundColor = [UIColor whiteColor];
+    return cell;
+}
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake((CGRectGetWidth(self.view.frame)/3), (CGRectGetWidth(self.view.frame))/3 );
+    
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0.0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 0.0;
+}
+
+// Layout: Set Edges
+- (UIEdgeInsets)collectionView:
+(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(0,0,0,0);  // top, left, bottom, right
+}
+
+
 @end
